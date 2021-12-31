6bbd2938009304e7f4c047bf3b4d43995c3d2013
Ant <it@me.org>
2021-12-31 06:32:53 -0600

    Init repo

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Initial Flake file";
+
+  inputs = {
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            (with dotnetCorePackages; combinePackages [
+              dotnet-sdk_6
+              dotnet-sdk_5
+              dotnetPackages.Nuget
+            ])
+          ];
+        };
+      });
+}
